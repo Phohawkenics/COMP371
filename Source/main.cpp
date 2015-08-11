c21b0429ae71d71d3cc0b99bca6d1c1522f253d3
julianlai777 <julianlai.777@gmail.com>
2015-08-11 11:29:51 -0400

    Added a second maze level

--- Source/main.cpp
+++ Source/main.cpp
@@ -36,9 +36,10 @@ int main(int argc, char*argv[])
 //		world.LoadScene("Scenes/CoordinateSystem.scene");
 #else
 //		world.LoadScene("../Assets/Scenes/StupidSimple.scene");
-		world.LoadScene("../Assets/Scenes/AnimatedSceneWithParticles.scene");
+//		world.LoadScene("../Assets/Scenes/AnimatedSceneWithParticles.scene");
 //		world.LoadScene("../Assets/Scenes/AnimatedScene.scene");
 //		world.LoadScene("../Assets/Scenes/MazeScene.scene");
+		world.LoadScene("../Assets/Scenes/MazeScene2.scene");
 //		world.LoadScene("../Assets/Scenes/StaticScene.scene");
 //		world.LoadScene("../Assets/Scenes/CoordinateSystem.scene");
 #endif
